5499097da1d58f6c2e5e76b5b4959c26cab5fd8f
Andres <ae@qbkconsulting.com>
2017-12-29 01:06:22 -0300

    Front Deploy

--- Angular/Software_para_organizacion_de_oficina/Back/src/main/java/spring/ProjectConstants.java
+++ Angular/Software_para_organizacion_de_oficina/Back/src/main/java/spring/ProjectConstants.java
@@ -6,10 +6,10 @@ public class ProjectConstants {
 	public static String PROJECT_NAME = "Office Manager";
 	public static String PROJECT_SMALL_NAME = "Office_Manager";
 
-	public static String PROJECT_VERSION = "1.0";
+	public static String PROJECT_VERSION = "1.2";
 
 	public static boolean PROJECT_MODE_MOCK = false;
 
-	public final static boolean HIDE_SWAGGER_OP = false;
+	public final static boolean HIDE_SWAGGER_OP = true;
 	
 }
