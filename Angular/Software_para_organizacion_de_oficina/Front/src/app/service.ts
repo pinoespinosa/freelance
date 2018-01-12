1b6dfbe972527c5c37a9ebcb45d393bed2a13428
Valentina <anavalentinaalfonso@gmail.com>
2018-01-11 22:47:30 -0300

    cambios varios, erroes arreglados

--- Angular/Software_para_organizacion_de_oficina/Front/src/app/service.ts
+++ Angular/Software_para_organizacion_de_oficina/Front/src/app/service.ts
@@ -19,9 +19,9 @@ import { Asesor }                 from 'app/data-objects/asesor';
 export class Service {
 
 
-  private server = 'http://18.216.175.95:8080/spring-security-rest/'
+  //private server = 'http://18.216.175.95:8080/spring-security-rest/'
   //private server = 'http://192.168.1.4:8080/officemanager/'
-  //private server = 'http://localhost:8080/officemanager/'
+  private server = 'http://localhost:8080/officemanager/'
 
   getServer(): string{
     return this.server;
